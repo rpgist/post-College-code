de2823a695770259fefbdacb155e64f762735a6c
Hawk <hawk@IT42.athena.com>
2019-05-23 17:01:03 -0400

    fixed adding to the file and added comments

--- find.java
+++ find.java
@@ -1,12 +1,17 @@
+//finds a string in a file and adds depending on user input
 import java.util.Scanner;
 import java.io.*;
 public class find{
     public static void main(String [] args)throws FileNotFoundException{
+	//string to hold current input
     	String input="";
+	//string to check. might change to boolean
 	String run = "y";
+	//gets file to use
 	System.out.print("Enter a file name: ");
 	Scanner sc = new Scanner(System.in);
 	String fileName =sc.next();
+	//makes sure file exists
 	try{
 	    File dir = new File(fileName);
 	    while(!dir.exists()){
@@ -15,7 +20,9 @@ public class find{
 		fileName=sc.next();
 		dir= new File(fileName);
 	    }
+	    //runs the program until user says to stop
 	    while(run.equals("y")){
+		//gets string to check
 	        System.out.print("Enter a string to search: ");
 	        input=sc.next().toLowerCase();
 	        boolean test =checker(input, dir);
@@ -37,6 +44,7 @@ public class find{
 	}
     }
     
+    //checks to see if the string is in the file
     public static boolean checker(String input, File dir)throws FileNotFoundException{
 	Scanner fileSc= new Scanner(dir);
 	while(fileSc.hasNextLine()){
@@ -51,10 +59,15 @@ public class find{
 	}
 	return false;
     }
-
-    public static void adder(String input, File dir)throws FileNotFoundException{
-        PrintStream ps = new PrintStream(dir);
-	ps.append(input);
-	ps.close();
+    
+    //adds the string to the file
+    public static void adder(String input, File dir){
+	try{
+    	    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(dir, true)));
+	    out.println(input);
+	    out.close();
+	}catch(Exception e){
+	    System.out.println(e);
+	}
     }
 }
