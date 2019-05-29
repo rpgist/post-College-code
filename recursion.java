107ad189eccd27baf68e22110fb3addec9aed946
Hawk <hawk@IT42.athena.com>
2019-05-28 22:12:38 -0400

    made a recursive fibonacci program. currently prints the sequence backwards.

---
+++ recursion.java
@@ -0,0 +1,29 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+public class recursion{
+    public static void main(String [] args){
+    	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter a number of repetitions: ");
+	String temp = sc.next();
+	int input = Integer.parseInt(temp);
+	System.out.println("Series up to " +input+" :");
+	for(int i=1; i<=input;i++){
+	    System.out.print(recur(input-i)+ " ");
+
+	}
+	System.out.println();
+    }
+
+    public static int recur(int input){
+    	if(input==1 || input == 2){
+	    return 1;
+	}else if(input ==0){
+	    return 0;
+	}
+	
+	return recur(input-1) + recur(input-2);	    
+    }
+    
+}
