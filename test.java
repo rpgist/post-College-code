5034806e2096a38210cdd120929fe9a8cda5c874
Hawk <hawk@IT42.athena.com>
2019-08-06 16:55:54 -0400

    made a little test app off some java practice website. it only wanted the if statement but i made a program to take input and feed it to the algorithm to test

--- test.java
+++ test.java
@@ -4,11 +4,34 @@ import java.io.*;
 
 public class test{
     public static void main (String [] args){
-    	int count =0;
-	String temp ="";
+	boolean weekday;
+	boolean vacation;
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter a directory to unzip");
+	System.out.print("enter weekday y or n: ");
+	String temp = sc.next();
+	if(temp.equals("y")){
+	    weekday = true;
+	}else{
+	    weekday= false;
+	}
+	System.out.print("enter vacation y or n: ");
 	temp = sc.next();
-
+	if(temp.equals("y")){
+	    vacation = true;
+	}else{
+	    vacation = false;
+	}
+    	if(tester(weekday, vacation)){
+	    System.out.println("sleep");
+	}else{
+	    System.out.println("awaken");
+	}
     } 
+    public static boolean tester(boolean weekday, boolean vacation){
+    	if(!weekday || vacation){
+	    return true;
+	}else{
+	    return false;
+	}
+    }
 }
